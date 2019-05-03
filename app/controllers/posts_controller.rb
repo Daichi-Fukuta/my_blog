3ac72ba4104314994ed624a33cf1865bee3f7a60
Daichi-Fukuta <ddd.ddd777ff@gmail.com>
2019-05-03 10:44:55 +0900

    一覧機能作成完了！

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,13 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [ :show, :edit, :update, :destroy]
   def index
+    @posts = Post.all
+    @new_posts = Post.all #@new_post = Post.order(created_at: :desc).limit(5)
+    @author = Author.first
   end
 
   def show
-    @post = Post.find(params[:id])
+    
   end
 
   def new
@@ -21,22 +25,26 @@ class PostsController < ApplicationController
   end
 
   def edit
-    @post = Post.find(params[:id])
   end
 
   def update
-    @post = Post.find(params[:id])
     @post.update(post_params)
     redirect_to @post
   end
 
   def destroy
+    @post.destroy
+    redirect_to posts_path
   end
 
     # プライベートメソッドでストロングパラメータを作成
     private
   def post_params
     params.require(:post).permit(:title, :body, :category)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 
 end
